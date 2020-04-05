5a7a7c7df19f312a5ea8d9fe934c417ad257d3ec
vova-kuk <58760099+vova-kuk@users.noreply.github.com>
2020-04-05 23:11:19 +0300

    Update login.spec.js (#192)

--- modules/user/login/login.spec.js
+++ modules/user/login/login.spec.js
@@ -1,55 +1,25 @@
 import {expect}  from 'chai';
 import LoginPage from '../_page/LoginPage';
-import HomePage from '../../_page/HomePage';
 import {student} from  '../_data/user.data';
-import Menu from '../../_page/Menu';
 import Notification from '../../_page/Notification';
+import MainPage from "../../_page/MainPage";
+import {H1LoginPage} from "../_data/userRegistration.data";
 
 describe('LOGIN PAGE --POSITIVE', () => {
     before(() => {
-        HomePage.open();
-        HomePage.loginLink.click();
+        LoginPage.open();
+        MainPage.verifyElementText(MainPage.header, H1LoginPage)
     });
 
-    it('should wait correct Login Page title', () => {
-        browser.waitUntil(() => {
-            return Menu.h1.getText() === 'User Login'
-        }, 5000, 'expected header to be different after 3s');
+    it('button `Submit` should be disabled by default', () => {
+        expect(LoginPage.submitBtn.isEnabled()).false;
     });
 
-    it('should fill out email field', () => {
-        LoginPage.emailInput.setValue(student.email);
+    it('should login user us student', () => {
+        LoginPage.login(student);
     });
 
-    it('should fill out password field', () => {
-        LoginPage.passwordInput.setValue(student.password);
+    it('should get successful notification about user login in the system', () => {
+        Notification.successMsgDisplayed();
     });
-
-    it('should check password field hide with bullets', () => {
-        const actual = LoginPage.passwordInput.getAttribute('type');
-        const expected = 'password';
-        expect(actual).eq(expected);
-    });
-
-    it('should click Login button', () => {
-        LoginPage.submitBtn.click();
-    });
-
-    it('should check transfer to Profile Page', () => {
-        browser.waitUntil(() => {
-        return Menu.h1.getText() === 'Student PASV'
-    }, 5000, 'expected header to be different after 3s');
-});
-
-    it('should get positive login notification', () => {
-        $(Notification.success).isDisplayed();
-    });
-
-    //Wait until "Completed challenge" notification bug fixing
-    //it('should get correct text in notification', () => {
-        //const actual = LoginPage.notification.getText();
-        //const expected = 'Auth success';
-        //expect(actual).eq(expected);
-    //});
-
 });
