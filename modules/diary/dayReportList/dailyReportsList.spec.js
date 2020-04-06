fe065fb54cc00cf375e6a0420a7cbc112f1708e0
Liudmila Petkel <a553311@yahoo.ca>
2020-04-06 15:04:20 -0400

    Daily reports list (#200)
    * revised dailyReportsList.spec.js
    
    * minor fix
    
    * minor fix

--- modules/diary/dayReportList/dailyReportsList.spec.js
+++ modules/diary/dayReportList/dailyReportsList.spec.js
@@ -1,10 +1,7 @@
-import Menu from '../../_page/Menu';
-import Notification from '../../_page/Notification';
 import LoginPage from '../../user/_page/LoginPage';
 import LogoutPage from '../../user/_page/LogoutPage';
 import CreateDayReportPage from '../../diary/_page/CreateDayReportPage';
 import DailyReportsPage from '../_page/DailyReportsPageList';
-import { dailyReportList } from '../_data/dailyReportsList.data';
 import { student } from '../../user/_data/user.data';
 
 describe('DAILY REPORTS PAGE TESTING', () => {
@@ -15,35 +12,9 @@ describe('DAILY REPORTS PAGE TESTING', () => {
     }
   });
 
-  it('should click Diary button', () => {
-    const element = DailyReportsPage.diaryBtn;
-    element.click();
-  });
-
-  it('should show success notification', () => {
-    browser.waitUntil(() => Notification.title.isDisplayed());
-  });
-
-  it('should check that page has correct description', () => {
-    expect(Menu.h1.getText()).equal(dailyReportList.h1);
-  });
-
-  it('should check Create Day Report button is displayed', () => {
-    expect(DailyReportsPage.createBtn.isDisplayed()).true;
-  });
-
-  it('should check paragraph contains correct description', () => {
-    const actual = Menu.p.getText();
-    expect(actual).eq(dailyReportList.p);
-  });
-
-  it('should Create Day Report button be clickable', () => {
-    const actual = DailyReportsPage.createBtn.isClickable();
-    expect(actual).true;
-  });
-
-  it('should check 3 most recent reports exist in the list', () => {
-    DailyReportsPage.checkLast3Reports();
+  it('should click Diary button and check 3 latest reports', () => {
+   DailyReportsPage.diaryBtn.click();
+   DailyReportsPage.checkLast3Reports();
   });
 
   after('should logout', () => {
