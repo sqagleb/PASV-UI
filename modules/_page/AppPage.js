c0311df48e47a8eb04158a735dcac66f5f5424ca
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-04-07 13:54:36 -0400

    Added maximizeWindow() in open() AppPage.js refactor user registration.

--- modules/_page/AppPage.js
+++ modules/_page/AppPage.js
@@ -1,5 +1,6 @@
 export default class AppPage {
   open(path) {
+    browser.maximizeWindow();
     browser.url(path);
   }
 }
--- modules/user/registration/newUserRegistration.spec.js
+++ modules/user/registration/newUserRegistration.spec.js
@@ -3,8 +3,9 @@ import Notification from '../../_page/Notification';
 import RegistrationPage from '../_page/RegistrationPage';
 import { H1RegisterPage } from '../_data/userRegistration.data';
 import MainPage from '../../_page/MainPage';
+import { H1LoginPage } from '../_data/login.data';
 
-describe('USER REGISTRATION', () => {
+describe('NEW USER REGISTRATION', () => {
   before('should open Register Page, verify H1 (header)', () => {
     RegistrationPage.open();
     MainPage.verifyElementText(MainPage.header, H1RegisterPage);
@@ -14,8 +15,12 @@ describe('USER REGISTRATION', () => {
     expect(RegistrationPage.submitBtn.isEnabled()).false;
   });
 
-  it('should fill out all fields, click Submit Btn, verify page h1 (header)', () => {
-    RegistrationPage.newUserRegisterVerifyH1();
+  it('should fill out all fields, click Submit Btn', () => {
+    RegistrationPage.registerNewUser();
+  });
+
+  it('verify page h1 (header)', () => {
+    MainPage.verifyElementText(MainPage.header, H1LoginPage);
   });
 
   it('should get successful notification about user registration in the system', () => {
