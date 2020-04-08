f541aa93c5e6a0deb44d9e4ea2d8d6cb0e629a6f
Aikerima <matyevaa@gmail.com>
2020-04-07 22:08:16 -0700

    Added tests for groupLectures

--- modules/groups/groupLectures/groupLectures.spec.js
+++ modules/groups/groupLectures/groupLectures.spec.js
@@ -1,20 +1,61 @@
+import { expect } from 'chai';
+
 import GroupPage from '../_page/GroupPage';
 import LecturePage from '../_page/LecturePage';
 import GroupsListPage from '../_page/GroupsListPage';
-import { newLecture } from '../_data/groupLecture.data';
 import { admin, student } from '../../user/_data/user.data';
 import LoginPage from '../../user/_page/LoginPage';
+import { newLecture } from '../_data/groupLecture.data';
+
 
 describe('TESTING NEW LECTURE', () => {
   before(() => {
     GroupPage.createNewGroup(admin);
     LecturePage.createLecture(admin);
-    browser.waitUntil(() => LecturePage.lectureLink.getText() === newLecture.name);
     LoginPage.login(student);
   });
 
   it('should verify lecture is accessible from Group page', () => {
     GroupsListPage.open();
     GroupsListPage.testGroup.click();
+    browser.waitUntil(() => GroupsListPage.testLecture.isClickable());
+    GroupsListPage.testLecture.click();
   });
+
+  it('should verify the lecture can be marked as understood', () => {
+    GroupsListPage.understoodBtn.click();
+    expect(GroupsListPage.understoodText.getText() === newLecture.markAsUnderstood);
+  });
+
+  it('should verify the lecture can be liked', () => {
+    GroupsListPage.likeBtn.click();
+    browser.waitUntil(() => GroupsListPage.btnBadge.isDisplayed());
+  });
+
+  it('should verify the lecture can be disliked', () => {
+    GroupsListPage.dislikeBtn.click();
+    browser.waitUntil(() => GroupsListPage.btnBadge.isDisplayed());
+  });
+
+  it('should verify homework is sent for review', () => {
+    GroupsListPage.homeworkTextBox.click();
+    GroupsListPage.homeworkTextBox.setValue(newLecture.homework);
+    GroupsListPage.sendHomeworkBtn.click();
+    browser.waitUntil(() => GroupsListPage.newHomeworkBadge.isDisplayed());
+  });
+
+  it('should verify commenting the lecture', () => {
+    GroupsListPage.commentTextBox.click();
+    GroupsListPage.commentTextBox.setValue(newLecture.comment);
+    GroupsListPage.addCommentBtn.click();
+    browser.waitUntil(() => GroupsListPage.commentConfirmation.isDisplayed());
+  });
+
+  it('should verify refresh button is clickable', () => {
+    expect(GroupsListPage.refreshCommentBtn.isClickable()).true;
+  });
+
+  it('should verify add comment button is disabled without a comment',() => {
+    expect(GroupsListPage.disabledCommentBtn.isEnabled()).false;
+  })
 });
