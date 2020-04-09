4d56b1b907610c0763f59b602a0662194fa8fa05
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-04-09 14:15:36 -0400

    newUserRegistration.spec.js add test verify fields with no input will be highlighted in red newUserRegistrationIncorrectInput.spec.js add tests with insert incorrect data

--- modules/user/registration/newUserRegistration.spec.js
+++ modules/user/registration/newUserRegistration.spec.js
@@ -27,3 +27,55 @@ describe('NEW USER REGISTRATION', () => {
     Notification.successMsgDisplayed();
   });
 });
+
+describe('NEW USER REGISTRATION', () => {
+  before('should open Register Page, verify H1 (header)', () => {
+    RegistrationPage.open();
+    MainPage.verifyElementText(MainPage.header, H1RegisterPage);
+  });
+
+  it('verify First Name(0) field with no input will be highlighted in red', () => {
+    RegistrationPage.firstNameInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(0);
+  });
+
+  it('verify Last Name(1) field with no input will be highlighted in red', () => {
+    RegistrationPage.lastNameInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(1);
+  });
+
+  it('verify Cell phone number(2) field with no input will be highlighted in red', () => {
+    RegistrationPage.cellPhoneNumberInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(2);
+  });
+
+  it('verify Email(3) field with no input will be highlighted in red', () => {
+    RegistrationPage.emailInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(3);
+  });
+
+  it('verify Password(4) field with no input will be highlighted in red', () => {
+    RegistrationPage.passwordInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(4);
+  });
+
+  it('verify About(5) textarea with no input will be highlighted in red', () => {
+    RegistrationPage.aboutInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(5);
+  });
+
+  it('verify My goals(6) textarea with no input will be highlighted in red', () => {
+    RegistrationPage.myGoalsInput.click();
+    RegistrationPage.verifyFieldHighlightInRed(6);
+  });
+
+  it('verify Country(7) drop down with no options will be highlighted in red', () => {
+    RegistrationPage.countryOption.click();
+    RegistrationPage.verifyFieldHighlightInRed(7);
+  });
+
+  it('verify English level(8) drop down with no options will be highlighted in red', () => {
+    RegistrationPage.englishLevelOption.click();
+    RegistrationPage.verifyFieldHighlightInRed(8);
+  });
+});
