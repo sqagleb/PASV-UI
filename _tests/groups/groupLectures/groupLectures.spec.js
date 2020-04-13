a31b5f31a7cb948d6c7baadde8ea6046c5f0b75d
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-04-13 19:09:35 -0400

    - Each lecture has unique name - Methods moved to correct pages - Minor fixes and refactoring

--- _tests/groups/groupLectures/groupLectures.spec.js
+++ _tests/groups/groupLectures/groupLectures.spec.js
@@ -2,60 +2,64 @@ import { expect } from 'chai';
 
 import GroupPage from '../../../_pages/GroupPage';
 import LecturePage from '../../../_pages/LecturePage';
-import GroupsListPage from '../../../_pages/GroupsListPage';
-import { admin, student } from '../../../_data/_user.data';
 import LoginPage from '../../../_pages/LoginPage';
+import LogoutPage from "../../../_pages/LogoutPage";
+import { admin, student } from '../../../_data/_user.data';
 import { newLecture } from '../../../_data/groupLecture.data';
+import GroupsListPage from "../../../_pages/GroupsListPage";
 
+before(() => {
+  GroupPage.createNewGroup(admin);
+  LecturePage.createLecture(admin);
+  LoginPage.login(student);
+});
 
-describe('TESTING NEW LECTURE', () => {
-  before(() => {
-    GroupPage.createNewGroup(admin);
-    LecturePage.createLecture(admin);
-    LoginPage.login(student);
-  });
-
+describe('TESTING LECTURE FUNCTIONALITY', () => {
   it('should verify lecture is accessible from Group page', () => {
     GroupsListPage.open();
     GroupsListPage.testGroup.click();
-    browser.waitUntil(() => GroupsListPage.testLecture.isClickable());
-    GroupsListPage.testLecture.click();
+    browser.waitUntil(() => GroupPage.testLecture.isClickable());
+    GroupPage.testLecture.click();
   });
 
   it('should verify the lecture can be marked as understood', () => {
-    GroupsListPage.understoodBtn.click();
-    expect(GroupsListPage.understoodText.getText() === newLecture.markAsUnderstood).true;
+    LecturePage.understoodBtn.click();
+    expect(LecturePage.understoodText.getText() === newLecture.markAsUnderstood).true;
   });
 
   it('should verify the lecture can be liked', () => {
-    GroupsListPage.likeBtn.click();
-    browser.waitUntil(() => GroupsListPage.btnBadge.isDisplayed());
+    LecturePage.likeBtn.click();
+    browser.waitUntil(() => LecturePage.btnBadge.isDisplayed());
   });
 
   it('should verify the lecture can be disliked', () => {
-    GroupsListPage.dislikeBtn.click();
-    browser.waitUntil(() => GroupsListPage.btnBadge.isDisplayed());
+    LecturePage.dislikeBtn.click();
+    browser.waitUntil(() => LecturePage.btnBadge.isDisplayed());
   });
 
   it('should verify homework is sent for review', () => {
-    GroupsListPage.homeworkTextBox.click();
-    GroupsListPage.homeworkTextBox.setValue(newLecture.homework);
-    GroupsListPage.sendHomeworkBtn.click();
-    browser.waitUntil(() => GroupsListPage.newHomeworkBadge.isDisplayed());
+    LecturePage.homeworkTextBox.click();
+    LecturePage.homeworkTextBox.setValue(newLecture.homework);
+    LecturePage.sendHomeworkBtn.click();
+    browser.waitUntil(() => LecturePage.newHomeworkBadge.isDisplayed());
   });
 
   it('should verify commenting the lecture', () => {
-    GroupsListPage.commentTextBox.click();
-    GroupsListPage.commentTextBox.setValue(newLecture.comment);
-    GroupsListPage.addCommentBtn.click();
-    browser.waitUntil(() => GroupsListPage.commentConfirmation.isDisplayed());
+    LecturePage.commentTextBox.click();
+    LecturePage.commentTextBox.setValue(newLecture.comment);
+    LecturePage.addCommentBtn.click();
+    browser.waitUntil(() => LecturePage.commentConfirmation.isDisplayed());
   });
 
   it('should verify refresh button is clickable', () => {
-    expect(GroupsListPage.refreshCommentBtn.isClickable()).true;
+    expect(LecturePage.refreshCommentBtn.isClickable()).true;
   });
 
   it('should verify add comment button is disabled without a comment',() => {
-    expect(GroupsListPage.disabledCommentBtn.isEnabled()).false;
+    expect(LecturePage.disabledCommentBtn.isEnabled()).false;
   })
+});
+
+after('should logout', ()=>{
+  LogoutPage.logout();
 });
